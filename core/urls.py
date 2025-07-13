a2506d6dcc06f3534c3281ab25e8b41a8bf3b106
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2025-07-12 19:23:32 -0700

    Add watchlist create form

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from core.views import (
     test_view,
     HomepageView,
+    WatchlistCreateView,
+    WatchlistDetailView,
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='index'),
+    path('watchlist/create', WatchlistCreateView.as_view(), name='watchlist_create'),
+    path('watchlist/<int:pk>', WatchlistDetailView.as_view(), name='watchlist_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.shortcuts import render, reverse
+from django.views.generic import TemplateView, View, CreateView
+
+from core.models import Watchlist
 
 
 def test_view(request, *args, **kwargs):
@@ -15,3 +17,26 @@ class HomepageView(View):
                 'watchlists': watchlists,
             })
         return render(request, 'core/index.html')
+
+
+class WatchlistCreateView(CreateView):
+    template_name = 'core/watchlist_create.html'
+    model = Watchlist
+    fields = ['name',]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('core:watchlist_detail', kwargs={'pk': self.object.pk})
+
+
+class WatchlistDetailView(TemplateView):
+    template_name = 'core/watchlist_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        watchlist_id = self.kwargs.get('pk')
+        context['watchlist'] = Watchlist.objects.get(pk=watchlist_id)
+        return context
